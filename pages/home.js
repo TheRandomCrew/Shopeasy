2f0aaec5694f255b96947f8728270f0a4ffbc74e
mikenath223 <michgoldennathan17@gmail.com>
2020-06-10 08:56:06 +0100

    [Update] Include top section image and intro styling on home

--- pages/home.js
+++ pages/home.js
@@ -4,21 +4,25 @@ import style from './css/home.module.css';
 const Home = () => {
   return (
     <div>
-      <nav>
-        <h1>ShopEasy</h1>
-        <ul>
-          <li>Men</li>
-          <li>Women</li>
-          <li>Kids</li>
-          <li></li>
-        </ul>
-      </nav>
-      <section className={style.headSect}>
-        <div>
-          <h1>Eqt support shoes</h1>
-        </div>
-        <img src='/shoe-head.png' alt="stock-shoe" />
-      </section>
+      <div className={style.wrap}>
+        <nav>
+          <h1>ShopEasy</h1>
+          <ul>
+            <li>Men</li>
+            <li>Women</li>
+            <li>Kids</li>
+            <li></li>
+          </ul>
+        </nav>
+        <section className={style.headSect}>
+          <div>
+            <h1>SS90</h1>
+            <h1>Collection</h1>
+            <p>Redefining Excellence</p>
+          </div>
+          <img src='/shoe-head.png' alt="stock-shoe" />
+        </section>
+      </div>
       <Footer />
     </div>
   )
