4e26c72c415e336edd20977cff5edc497550118e
mikenath223 <michgoldennathan17@gmail.com>
2020-07-05 00:32:01 +0100

    [Feature] Include footer section and style

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,16 @@
+import style from '../view/css/home.module.css';
+
 const Footer = () => (
-  <div>
-    <h2>Footer</h2>
+  <div className={style.footer}>
+    <nav>
+      <h3>&copy; 2020 ShopEasy All rights reserved</h3>
+      <ul>
+        <li>Accessibility Policy</li>
+        <li>Privacy Policy</li>
+        <li>Terms of Use</li>
+        <li>#RunForGood</li>
+      </ul>
+    </nav>
   </div>
 )
 
